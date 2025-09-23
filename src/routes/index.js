b02b8d1baa54ac157967234ed3f348b28c68a920
Naveed Ahmed Syed <syednaveed2006@icloud.com>
2025-09-22 21:57:59 -0400

    Failing to see elsint steps

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { authenticate } = require('../auth');
 
+//failing eslint on purpose...
+const unneededVariable = 'This variable is never used';
+
 router.use('/v1', authenticate(), require('./api'));
 
 router.get('/', (req, res) => {
