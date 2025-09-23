26aa730d0adb69261abd19b9223a4cf37ea20c2b
Naveed Ahmed Syed <syednaveed2006@icloud.com>
2025-09-22 22:05:01 -0400

    Fixed the eslint again to pass the action

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { authenticate } = require('../auth');
 
-//failing eslint on purpose...
-const unneededVariable = 'This variable is never used';
-
 router.use('/v1', authenticate(), require('./api'));
 
 router.get('/', (req, res) => {
